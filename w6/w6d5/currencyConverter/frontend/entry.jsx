ac6a47498472aee6bbfad94f34e4bcefa9b57f01
Matt McGregor <mttmcgrgr@gmail.com>
2017-01-01 17:58:46 -0800

    add react /redux practice projects

---
+++ w6/w6d5/currencyConverter/frontend/entry.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Widget from './components/widget';
+import store from './store';
+import { selectCurrency } from './actions';
+import reducer from "./reducer";
+
+// window.selectCurrency = selectCurrency;
+// window.reducer = reducer;
+
+document.addEventListener("DOMContentLoaded", function(){
+  ReactDOM.render(<Widget store={store} />, document.getElementById('root'));
+});
